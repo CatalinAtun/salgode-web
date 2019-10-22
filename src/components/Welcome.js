d45f58718a1f2aeeba171f4919790a91969b866a
Benjamín Thomas Kramm Pino <btkramm@uc.cl>
2019-10-22 00:42:48 -0300

    feat: add material-ui and an example of usage (#1)
    * feat: add material-ui and an example of usage
    
    * fix: fix bad export

--- src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Welcome = () => (
-  <>
-    <div>Bienvenido</div>
-  </>
-)
-
-export default Welcome
---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+import { Button } from '@material-ui/core'
+
+const Home = () => (
+  <div className="home">
+    <Button>Click me!</Button>
+  </div>
+)
+
+export default Home
